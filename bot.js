6cc85c003ad03ac1298774f527bc6845f7330769
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-08 23:34:04 +0100

    can read from s3

--- bot.js
+++ bot.js
@@ -1,8 +1,25 @@
 var botBuilder = require('claudia-bot-builder')
+var aws = require('aws-sdk')
+  var s3 = new aws.S3({signatureVersion: 'v4'})
 
 module.exports = botBuilder(function (request) {
-  return 'Thanks for sending ' + request.text  + 
-      '. Your message is very important to us, but '
+  console.log("start reading")
+  return new Promise((resolve, reject) => {
+      s3.getObject({
+        Bucket: "hots-builds",
+        Key: "azmodan.json"
+      }, callback(resolve, reject))
+    })
 },{
   platforms: ['telegram']
 });
+
+function callback(resolve, reject) {
+    return function(err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data.Body.toString('ascii'))
+      }
+    }
+  }
