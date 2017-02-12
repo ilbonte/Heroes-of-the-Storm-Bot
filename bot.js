f411c365f64d02b81aef31abb09a3fc81ac15cba
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-12 20:49:36 +0100

    move bot to differnt file

--- bot.js
+++ bot.js
@@ -1,25 +1,6 @@
 var botBuilder = require('claudia-bot-builder')
-var aws = require('aws-sdk')
-  var s3 = new aws.S3({signatureVersion: 'v4'})
+const hotsBot = require('./lib/hotsBot')
 
-module.exports = botBuilder(function (request) {
-  console.log("start reading")
-  return new Promise((resolve, reject) => {
-      s3.getObject({
-        Bucket: "hots-builds",
-        Key: "azmodan.json"
-      }, callback(resolve, reject))
-    })
-},{
+module.exports = botBuilder(hotsBot,{
   platforms: ['telegram']
 });
-
-function callback(resolve, reject) {
-    return function(err, data) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data.Body.toString('ascii'))
-      }
-    }
-  }
