b758e147da985735000975a5f615ca738d1f9167
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-18 16:02:31 +0100

    add basic build printer

--- test/PrinterTest.js
+++ test/PrinterTest.js
@@ -1,5 +1,40 @@
 const assert = require('assert')
+const Printer = require('../lib/Printer')
 
-test ("test", function(){
-  assert.equal(1,1)
+const HERO_WITH_TWO_BUILDS = require('./fixtures/azmodan.json')
+
+test("can print an hero's builds from builds array", function () {
+  var printer = new Printer(HERO_WITH_TWO_BUILDS)  
+  var expected = `All Shall Burn Build
+1 | 3
+4 | 1
+7 | 4
+10| 1
+13| 3
+16| 2
+20| 4
+
+Taste For Blood Build
+1 | 1
+4 | 2
+7 | 1
+10| 2
+13| 3
+16| 2
+20| 4
+
+`;
+
+  var result = printer.buildsAsText()
+
+  assert.equal(expected, result)
+})
+
+test("can print an informative message if the build can't be found", function(){
+  var HERO_WITH_NO_BUILDS = []
+  var printer = new Printer(HERO_WITH_NO_BUILDS)  
+  
+  var result = printer.buildsAsText()
+
+  assert.equal("No builds found", result)
 })
